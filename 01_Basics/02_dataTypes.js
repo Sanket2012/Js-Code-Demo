b7fee7e9dfd22585c56587c3db391cff58271060
Sanket2012 <45317536+Sanket2012@users.noreply.github.com>
2024-07-30 02:30:25 +0000

    Commiting dataTypes in basics

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all JS code as newer version
+let age = 18
+
+//alert("Hello") // we are using NodeJs, not browser
+
+/* Data Types : 
+Premetives : 
+    number
+    bigInt
+    string
+    boolean
+    null => standalone value
+    undefined => not yet assigned any value to the variables
+    symbol => to find uniquness
+
+object
+*/
+
+console.log(typeof null)
+console.log(typeof undefined)
